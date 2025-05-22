dfb96f6af15b3666386357235e5c21ae7e39e5ff
Sam Trouy <smltr@proton.me>
2025-05-22 10:01:09 -0400

    only us servers, add cron job

--- api/servers.go
+++ api/servers.go
@@ -1,23 +1,17 @@
 package api
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
-
-	"findservers/models"
 )
 
 func GetServers(w http.ResponseWriter, r *http.Request) {
 	println("getting servers...")
 
-	// Direct URL to the blob
-	blobURL := os.Getenv("BLOB_URL") + "/servers.json"
+	blobURL := os.Getenv("BLOB_URL") + "/servers.txt"
 
-	// Create a new request
 	req, err := http.NewRequest("GET", blobURL, nil)
 	if err != nil {
 		fmt.Println("Error creating request:", err)
@@ -25,13 +19,11 @@ func GetServers(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Add auth token if needed
 	apiKey := os.Getenv("BLOB_READ_WRITE_TOKEN")
 	if apiKey != "" {
 		req.Header.Add("Authorization", "Bearer "+apiKey)
 	}
 
-	// Send the request
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
@@ -41,7 +33,6 @@ func GetServers(w http.ResponseWriter, r *http.Request) {
 	}
 	defer resp.Body.Close()
 
-	// Check if response is successful
 	if resp.StatusCode != http.StatusOK {
 		errMsg := fmt.Sprintf("Failed to fetch blob: status code %d", resp.StatusCode)
 		fmt.Println(errMsg)
@@ -49,26 +40,15 @@ func GetServers(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Read the response body
-	jsonBytes, err := io.ReadAll(resp.Body)
+	htmlContent, err := io.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println("Error reading response:", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	println("unmarshalling...")
-	// Create blob into io reader
-	reader := bytes.NewReader(jsonBytes)
+	w.Header().Set("Content-Type", "text/html")
 
-	// try to unmarshal to json
-	var servers []models.Server
-	if err := json.NewDecoder(reader).Decode(&servers); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	// write response
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(servers)
+	w.WriteHeader(http.StatusOK)
+	w.Write(htmlContent)
 }
